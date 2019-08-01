631908263baed888108fd21888a82a23187058cf
vishalk2899 <37765286+vishalk2899@users.noreply.github.com>
2019-08-01 21:34:41 +0530

    Create ml1.py

---
+++ ml1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+a=[]
+n=int(input("Enter the number of elements:"))
+
+for i in range(0,n):
+  element=int(input("Enter element"+str(i+1)+":"))
+  a.append(element)
+  
+x=pd.Series(a)
+
+mean=x.mean()
+median=x.median()
+mode=x.mode()
+std=x.std()
+
+print("Mean",mean)
+print("Median",median)
+print("Mode",mode)
+print("Standard Deviation",std)
